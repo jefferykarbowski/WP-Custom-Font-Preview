029dbdfe7a635c6e5df1f17a062840ad4c28ebb6
jefferykarbowski <58263127+jefferykarbowski@users.noreply.github.com>
2022-02-12 10:09:44 -0500

    Updated version

--- wp-custom-font-preview.php
+++ wp-custom-font-preview.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WP Custom Font Preview
  * Plugin URI:        https://jefferykarbowski.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Jeffery Karbowski
  * Author URI:        https://jefferykarbowski.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WP_CUSTOM_FONT_PREVIEW_VERSION', '1.0.0' );
+define( 'WP_CUSTOM_FONT_PREVIEW_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
